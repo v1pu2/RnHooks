dc08741aca9136127cf9d0e47e07d2d745c09f69
Vipu-sa <vipula.jivani.sa@gmail.com>
2022-04-14 12:42:41 +0530

    demo of useEffect

--- src/screens/UseEffectHook.js
+++ src/screens/UseEffectHook.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 
 import {StyleSheet, Text, View} from 'react-native';
 
 const UseEffectHook = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/comments')
+      .then(response => {
+        console.log('response');
+        setData(response?.data);
+      });
+  }, []);
+
   return (
     <View style={styles.root}>
-      <Text>this is use effect UseEffectHook</Text>
+      <Text>Email : {data[0]?.email}</Text>
     </View>
   );
 };
@@ -14,6 +26,7 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
