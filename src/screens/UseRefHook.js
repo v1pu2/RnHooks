79b17fd46120d25453e1e0fb1cab84f6ebf50000
Vipu-sa <vipula.jivani.sa@gmail.com>
2022-04-14 14:54:35 +0530

    demo of useRef

--- src/screens/UseRefHook.js
+++ src/screens/UseRefHook.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 const UseRefHook = () => {
+  const inputRef = useRef(null);
+  const onClick = () => {
+    inputRef.current.focus();
+    console.log('inupt ref val', inputRef.current.value);
+  };
+  const onChange = value => {
+    inputRef.current = value;
+  };
   return (
     <View style={styles.root}>
-      <Text>this is use ref hook</Text>
+      <Text>Pedro</Text>
+      <View style={styles.rowView}>
+        <TextInput
+          placeholder="Enter name"
+          style={styles.input}
+          ref={inputRef}
+          onChange={val => onChange(val)}
+        />
+        <Button title="change name" onPress={() => onClick()} />
+      </View>
     </View>
   );
 };
@@ -14,6 +31,17 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rowView: {
+    flexDirection: 'row',
+    margin: 20,
+  },
+  txtStyle: {fontSize: 22},
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    flex: 1,
   },
 });
 
