0fb1d0f64da181ab3f8c922f171d83da33ab6fc2
Vipu-sa <vipula.jivani.sa@gmail.com>
2022-04-15 11:46:50 +0530

    add usecase

--- src/screens/UseImperativeHandleHook.js
+++ src/screens/UseImperativeHandleHook.js
@@ -3,6 +3,7 @@ import React, {useRef} from 'react';
 import {StyleSheet, View, Button} from 'react-native';
 import ButtonChild from '../component/ButtonChild';
 
+// useCase => call button click with ref from parent for child class. with ref get the current method and change the state value of child from parent
 const UseImperativeHandleHook = () => {
   const buttonRef = useRef(null);
   return (
